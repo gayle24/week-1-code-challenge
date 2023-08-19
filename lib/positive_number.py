5a22214079f5b2555c5f68a3d2dfc8d9ee4a888d
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-08-20 02:10:08 +0300

    positive number checker compllete

---
+++ lib/positive_number.py
@@ -0,0 +1,14 @@
+def positive_number(a, b, c):
+    positive_count = 0
+
+    if a > 0:
+        positive_count += 1
+    if b > 0:
+        positive_count += 1
+    if c > 0:
+        positive_count += 1
+
+    return positive_count == 2
+
+# Example 
+print(positive_number(2,5,-4))
